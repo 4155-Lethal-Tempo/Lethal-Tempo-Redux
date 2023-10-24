cd06a49695e0c9e0ee9a21b704cb434cf4d77df4
Cesar Guzman <cesar.castroguz@gmail.com>
2023-10-24 17:04:27 -0400

    Changed Authorization in refresh to include env vars

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.get('/refresh_token', function(req, res) {
     var refresh_token = req.query.refresh_token;
     var authOptions = {
       url: 'https://accounts.spotify.com/api/token',
-      headers: { 'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')) },
+      headers: { 'Authorization': 'Basic ' + (new Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
       form: {
         grant_type: 'refresh_token',
         refresh_token: refresh_token
