3ac1ad49c24dc0062633a04ba78fa30c38e88fee
CesarCGuzman <cesar.castroguz@gmail.com>
2023-11-03 16:07:44 -0400

    re-adding project due to github issues

--- app.js
+++ app.js
@@ -7,13 +7,17 @@
 
 const express = require('express');
 const app = express();
+const request = require('request');
 const querystring = require('node:querystring');
 require('dotenv').config();
 const port = 8084;
+const fs = require('fs')
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+app.use(express.static('public'));
 
 // This page contains a link that sends you to the Spotify login page
 app.get('/', (req, res) => {
@@ -21,10 +25,9 @@ app.get('/', (req, res) => {
 });
 
 // When you click on said line you are sent to this
-var state;
 app.get('/login', (req, res) => {
     var state = generateRandomString(16);
-    var scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-read user-follow-modify user-top-read user-read-recently-played user-read-playback-position';
+    var scope = 'ugc-image-upload user-read-private user-read-email playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-read user-follow-modify user-top-read user-read-recently-played user-read-playback-position';
 
     res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
@@ -39,9 +42,8 @@ app.get('/login', (req, res) => {
 // This is the callback page that is called after you login
 // It is supposed to give you an access token and a refresh token
 // We'll use this to make API calls
-var code;
-app.get('/callback', async (req, res) => {
-    code = req.query.code || null;
+app.get('/callback', function(req, res) {
+    var code = req.query.code || null;
     var state = req.query.state || null;
   
     if (state === null) {
@@ -58,14 +60,26 @@ app.get('/callback', async (req, res) => {
           grant_type: 'authorization_code'
         },
         headers: {
+          'content-type': 'application/x-www-form-urlencoded',
           'Authorization': 'Basic ' + (new Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
         },
         json: true
       };
     }
-    console.log(authOptions);
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        access_token = body.access_token;
+        process.env.ACCESS_TOKEN = access_token;
+      } else {
+        console.log(response.body);
+        console.log(response.statusMessage);
+      }
+    });
     res.redirect('/home');
 });
+
+
 
 app.get('/home', (req, res) => {
     res.render('main/landingPage.ejs');
@@ -73,15 +87,23 @@ app.get('/home', (req, res) => {
 app.get('/topTracks', (req, res) => {
   res.render('main/topTracks.ejs');
 });
+app.get('/topPodcasts', (req, res) => {
+  res.render('main/topPodcasts.ejs');
+});
 
 var access_token;
 // Refreshes the access token in case it expires
 app.get('/refresh_token', function(req, res) {
 
     var refresh_token = req.query.refresh_token;
+    var authString = process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET;
+    
     var authOptions = {
       url: 'https://accounts.spotify.com/api/token',
-      headers: { 'Authorization': 'Basic ' + (new Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + authString.toString('base64') 
+      },
       form: {
         grant_type: 'refresh_token',
         refresh_token: refresh_token
@@ -93,8 +115,10 @@ app.get('/refresh_token', function(req, res) {
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
         access_token = body.access_token;
+        refresh_token = body.refresh_token;
         res.send({
-          'access_token': access_token
+          'access_token': access_token,
+          'refresh_token': refresh_token
         });
       }
     });
@@ -110,49 +134,67 @@ function generateRandomString(length) {
         const randomIndex = Math.floor(Math.random() * charactersLength);
         result += characters.charAt(randomIndex);
     }
-
     return result;
 }
 
-
-if (typeof localStorage === "undefined" || localStorage === null) {
-  var LocalStorage = require('node-localstorage').LocalStorage;
-  localStorage = new LocalStorage('./scratch');
-}
-
-localStorage.setItem('myFirstKey', 'myFirstValue');
-console.log(localStorage.getItem('myFirstKey'));
-const fetch = require('node-fetch');
-
-app.get('/top-tracks', async (req, res) => {
-  res.redirect('/topTracks');
-  //localStorage.getItem('access_token')
-  let accessToken = 'BQBm-voZyybCz0-y1dAi8QFpdFrvL9WHLqQpaXhSak3jUIdM6utX1vnHIgz4SZscPT2fQordyNcv1z2pBKplo0uX0ABNx3vNCp8fa96_xQ9qV9M6Iwzff1_RJ-aNoucY__ha_nfYswm18g6f8OpCiRaEwacFDj44j5UdD4s8-FPrn9UCcNTN0jPjmq03JNLBr-s2x6Note2YGSQvo_q39m9r235F1ACOa3HLm_9KCpW9uqJQtQni27g2caNP2cBtR6gdMO9b';
-  const token = accessToken;
-  async function fetchWebApi(endpoint, method, body) {
-    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      method,
-      body:JSON.stringify(body)
-    });
-    return await res.json();
-  }
   
-  async function getTopTracks(){
-    // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
-    return (await fetchWebApi(
-      'v1/me/top/tracks?time_range=long_term&limit=25', 'GET'
-    )).items;
-  }
   
-  const topTracks = await getTopTracks();
-  console.log(
-    topTracks?.map(
-      ({name, artists}) =>
-        `${name} by ${artists.map(artist => artist.name).join(', ')}`
-    )
-  );
-
+  app.get('/top-podcasts', async (req, res) => {
+    res.redirect('/topPodcasts');
+    
   });
+
+  app.get('/top-tracks', async (req, res) => {
+      let accessToken = process.env.ACCESS_TOKEN;
+    try {
+    const response = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=50',{
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+
+      
+    });
+
+    const shortTermResponse = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50', {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+
+    const mediumTermResponse = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50', {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+
+  
+
+    if (response.status === 200 && shortTermResponse.status === 200 && mediumTermResponse.status === 200) {
+      const data = await response.json();
+      const shortTermData = await shortTermResponse.json();
+      const mediumTermData = await mediumTermResponse.json();
+
+      res.render('main/topTracks.ejs', { tracks: data, shortTermTracks: shortTermData, mediumTermTracks: mediumTermData});
+    } else {
+      res.send('Failed to retrieve top tracks. Status code: ' + response.status);
+    }
+
+    /*
+    if (shortTermResponse.status === 200) {
+      const data = await shortTermResponse.json();
+      console.log(data); // Log the response data for debugging
+      res.render('main/topTracks.ejs', { trackss: data });
+    } else {
+      res.send('Failed to retrieve top tracks. Status code: ' + shortTermResponse.status);
+    }
+    */
+
+
+  } catch (error) {
+    res.send('An error occurred: ' + error.message);
+  }
+});
+
+
+
+  // -------------------------------//
