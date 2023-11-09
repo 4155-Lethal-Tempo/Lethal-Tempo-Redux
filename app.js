79c7530ee623c91860cd6486936276ac88633703
Cesar Guzman <cesar.castroguz@gmail.com>
2023-11-09 00:06:36 -0500

    Added code to referesh the token

--- app.js
+++ app.js
@@ -57,6 +57,8 @@ app.get('/callback', function(req, res) {
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
         req.session.access_token = body.access_token;
+        req.session.refresh_token = body.refresh_token;
+        req.session.access_token_received_at = Date.now();
         res.redirect('/');
       } else {
         console.log(response.body);
@@ -114,7 +116,7 @@ app.get('/topPodcasts', (req, res) => {
 // Refreshes the access token in case it expires
 app.get('/refresh_token', function(req, res) {
 
-    var refresh_token = req.query.refresh_token;
+    var refresh_token = req.session.refresh_token;
     var authString = process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET;
     
     var authOptions = {
@@ -143,6 +145,17 @@ app.get('/refresh_token', function(req, res) {
     });
 });
 
+function accessTokenHasExpired(req) {
+  // Assume the access token expires in 1 hour (3600 seconds)
+  const EXPIRATION_TIME = 3600 * 1000; // Convert to milliseconds
+
+  // Get the time the access token was received from the session
+  const accessTokenReceivedAt = req.session.access_token_received_at;
+
+  // Check if the current time is past the expiration time of the access token
+  return Date.now() > accessTokenReceivedAt + EXPIRATION_TIME;
+}
+
 // Generates a random string containing numbers and letters - used for state
 function generateRandomString(length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -164,58 +177,68 @@ function generateRandomString(length) {
   });
 
   app.get('/top-tracks', async (req, res) => {
-    try {
-    const response = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=50',{
-      headers: {
-        'Authorization': `Bearer ${req.session.access_token}`
+    if(accessTokenHasExpired(req)){
+      try {
+        const { data } = await axios.get('/refresh_token');
+        req.session.access_token = data.access_token;
+        req.session.access_token_received_at = Date.now();
+      } catch (error) {
+          console.error('Error refreshing access token', error);
+          // Handle the error appropriately in your application
       }
-
+    } else {
+      try {
+        const response = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=50',{
+          headers: {
+            'Authorization': `Bearer ${req.session.access_token}`
+          }
+    
+          
+        });
+    
+        const shortTermResponse = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50', {
+          headers: {
+            'Authorization': `Bearer ${req.session.access_token}`
+          }
+        });
+    
+        const mediumTermResponse = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50', {
+          headers: {
+            'Authorization': `Bearer ${req.session.access_token}`
+          }
+        });
+    
       
-    });
-
-    const shortTermResponse = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50', {
-      headers: {
-        'Authorization': `Bearer ${req.session.access_token}`
+    
+        if (response.status === 200 && shortTermResponse.status === 200 && mediumTermResponse.status === 200) {
+          const data = await response.json();
+          const shortTermData = await shortTermResponse.json();
+          const mediumTermData = await mediumTermResponse.json();
+    
+          res.render('main/topTracks.ejs', { 
+            tracks: data, 
+            shortTermTracks: shortTermData, 
+            mediumTermTracks: mediumTermData
+          });
+        } else {
+          res.send('Failed to retrieve top tracks. Status code: ' + response.status);
+        }
+    
+        /*
+        if (shortTermResponse.status === 200) {
+          const data = await shortTermResponse.json();
+          console.log(data); // Log the response data for debugging
+          res.render('main/topTracks.ejs', { trackss: data });
+        } else {
+          res.send('Failed to retrieve top tracks. Status code: ' + shortTermResponse.status);
+        }
+        */
+    
+    
+      } catch (error) {
+        res.send('An error occurred: ' + error.message);
       }
-    });
-
-    const mediumTermResponse = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50', {
-      headers: {
-        'Authorization': `Bearer ${req.session.access_token}`
-      }
-    });
-
-  
-
-    if (response.status === 200 && shortTermResponse.status === 200 && mediumTermResponse.status === 200) {
-      const data = await response.json();
-      const shortTermData = await shortTermResponse.json();
-      const mediumTermData = await mediumTermResponse.json();
-
-      res.render('main/topTracks.ejs', { 
-        tracks: data, 
-        shortTermTracks: shortTermData, 
-        mediumTermTracks: mediumTermData,
-        page: 'topTracks'
-      });
-    } else {
-      res.send('Failed to retrieve top tracks. Status code: ' + response.status);
     }
-
-    /*
-    if (shortTermResponse.status === 200) {
-      const data = await shortTermResponse.json();
-      console.log(data); // Log the response data for debugging
-      res.render('main/topTracks.ejs', { trackss: data });
-    } else {
-      res.send('Failed to retrieve top tracks. Status code: ' + shortTermResponse.status);
-    }
-    */
-
-
-  } catch (error) {
-    res.send('An error occurred: ' + error.message);
-  }
 });
 
 
